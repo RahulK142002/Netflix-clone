54146a0b92ecf0f1ca8024f635090c2911efe17c
Rahul <rahulkapadia2002@gmail.com>
2023-02-23 15:20:50 +0530

    first commit

---
+++ src/Nav.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Nav.css';
+
+
+
+
+    
+function Nav() {
+    
+    
+    return (
+    <div className="nav">
+        <img 
+        className='nav_logo'
+        src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png'
+        alt='Netflix Logo'
+        />
+        <img 
+        className='nav_avatar'
+        src='https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png'
+        alt='Netflix Logo'
+        />
+    </div>
+  )
+}
+
+export default Nav
